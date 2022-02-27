405a2cca46377092103db34853b71bb66ce789d6
Christian Berendt <berendt@osism.tech>
2022-02-27 18:25:10 +0100

    manager: remove awx
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- roles/manager/files/awx/provision-instance.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo 'from django.conf import settings; x = settings.AWX_TASK_ENV; x["HOME"] = "/var/lib/awx"; settings.AWX_TASK_ENV = x' | awx-manage shell
-awx-manage provision_instance --hostname=$(hostname) --node_type=control
