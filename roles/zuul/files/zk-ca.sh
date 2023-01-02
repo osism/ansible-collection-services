b90171bcfa6b7a67843a70cb152589f86da0fd96
Ramona Beermann <80961037+ra-beer@users.noreply.github.com>
2023-01-02 09:10:16 +0100

    Change copyright date (#910)
    Partly osism/issues#394
    
    Signed-off-by: Ramona Beermann <ramona.beermann@osism.tech>

--- roles/zuul/files/zk-ca.sh
+++ roles/zuul/files/zk-ca.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 # Copyright 2020 Red Hat, Inc
-# Copyright 2022 OSISM GmbH
+# Copyright 2022-2023 OSISM GmbH
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may
 # not use this file except in compliance with the License. You may obtain
