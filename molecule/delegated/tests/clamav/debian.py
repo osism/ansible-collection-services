cb2257625559ede2aef670fae36d6723a2a76363
Gondermann <gondermann@b1-systems.de>
2024-02-22 11:57:26 +0100

    Fix clamav role
    Signed-off-by: Gondermann <gondermann@b1-systems.de>

---
+++ molecule/delegated/tests/clamav/debian.py
@@ -0,0 +1,21 @@
+import pytest
+
+from ..util.util import get_ansible, get_variable, get_os_role_variable
+
+testinfra_runner, testinfra_hosts = get_ansible()
+
+
+def check_ansible_os_family(host):
+    if get_variable(host, "ansible_os_family", True) != "Debian":
+        pytest.skip("ansible_os_family mismatch")
+
+
+def test_pkg(host):
+    check_ansible_os_family(host)
+
+    package_names = get_os_role_variable(host, "clamav_package_names")
+    assert type(package_names) is list
+
+    for package_name in package_names:
+        package = host.package(package_name)
+        assert package.is_installed
