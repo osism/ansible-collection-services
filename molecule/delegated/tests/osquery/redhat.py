31af01114d77657bb951a49180beae6b7095dfae
Robin van der Linden <linden@b1-systems.de>
2024-02-27 15:55:36 +0100

    Adding osquery support for CentOS
    Signed-off-by: Robin van der Linden <linden@b1-systems.de>

---
+++ molecule/delegated/tests/osquery/redhat.py
@@ -0,0 +1,33 @@
+import pytest
+from ..util.util import get_ansible, get_variable, get_from_url, get_centos_repo_key
+
+testinfra_runner, testinfra_hosts = get_ansible()
+
+
+def check_ansible_os_family(host):
+    if get_variable(host, "ansible_os_family", True) != "RedHat":
+        pytest.skip("ansible_os_family mismatch")
+
+def test_osquery_gpgkey(host):
+    check_ansible_os_family(host)
+    osquery_configure_repository = get_variable(host, "osquery_configure_repository")
+
+    if not osquery_configure_repository:
+        pytest.skip("osquery_configure_repository is not true")
+
+    # Fetch the GPG key content from the osquery URL defined in Ansible variables
+    url = get_variable(host, "osquery_redhat_repository_key")
+    key_content = get_from_url(url)
+
+    # Use the get_centos_repo_key function with the appropriate summary for osquery
+    installed_key = get_centos_repo_key(
+        host, "osquery (osquery) <osquery@fb.com> public key"
+    )
+
+    # Verify that the installed key matches the fetched key
+    assert installed_key in key_content
+
+    # Verify the existence of the osquery repo file
+    repo_file = host.file("/etc/yum.repos.d/osquery.repo")
+    assert repo_file.exists
+    assert not repo_file.is_directory
