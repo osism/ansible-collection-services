cedd061039336cb95831788284aeed884f4dc9b4
Robin van der Linden <linden@b1-systems.de>
2024-02-28 15:40:18 +0100

    Add CentOS support for role netdata
    Signed-off-by: Robin van der Linden <linden@b1-systems.de>

---
+++ molecule/delegated/tests/netdata/debian.py
@@ -0,0 +1,25 @@
+import pytest
+from ..util.util import get_ansible, get_variable, get_from_url
+
+testinfra_runner, testinfra_hosts = get_ansible()
+
+
+def check_ansible_os_family(host):
+    if get_variable(host, "ansible_os_family", True) != "RedHat":
+        pytest.skip("ansible_os_family mismatch")
+
+
+def test_repository_configuration(host):
+    check_ansible_os_family(host)
+
+    if not get_variable(host, "netdata_configure_repository"):
+        pytest.skip("netdata_configure_repository is not true")
+
+    key_file = host.file("/etc/apt/trusted.gpg.d/netdata.asc")
+    key_content = get_from_url(get_variable(host, "netdata_debian_repository_key"))
+    assert key_file.exists
+    assert not key_file.is_directory
+    assert key_file.user == "root"
+    assert key_file.group == "root"
+    assert key_file.mode == 0o644
+    assert key_file.content_string == key_content
