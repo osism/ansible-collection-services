1e23cdbaccb4b0a62c4f5abf72c3fff937f9a6f0
lindenb1 <138122960+lindenb1@users.noreply.github.com>
2024-05-02 17:26:36 +0200

    Fix Ubuntu 24.04 roles (#1417)
    Signed-off-by: Robin van der Linden <linden@b1-systems.de>

--- molecule/delegated/tests/chrony/debian.py
+++ molecule/delegated/tests/chrony/debian.py
@@ -11,9 +11,17 @@ def check_ansible_os_family(host):
 
 
 def test_srv(host):
+    """Check if the systemd-timesyncd service is disabled."""
+
     check_ansible_os_family(host)
 
-    service_name = "systemd-timesyncd"
-    service = host.service(service_name)
-    assert not service.is_enabled
-    assert not service.is_running
+    service = host.service("systemd-timesyncd")
+    cmd = host.run(
+        f'systemctl list-units --all | grep -q "^[[:space:]]*{service.name}"'
+    )
+
+    if cmd.rc == 0:
+        assert not service.is_enabled, f"{service.name} should not be enabled"
+        assert not service.is_running, f"{service.name} should not be running"
+    else:
+        pytest.skip("The systemd-timesyncd service does not exist")
--- molecule/delegated/tests/fail2ban.py
+++ molecule/delegated/tests/fail2ban.py
@@ -1,9 +1,21 @@
+import pytest
 from .util.util import get_ansible, get_variable
 
 testinfra_runner, testinfra_hosts = get_ansible()
 
 
 def test_srv(host):
+    """
+    Temporary skipping on Ubuntu 24.04 as the service is currently unstable on 24.04
+    Also see: https://bugs.launchpad.net/ubuntu/+source/fail2ban/+bug/2055114
+    """
+
+    if (
+        get_variable(host, "ansible_distribution", True) == "Ubuntu"
+        and get_variable(host, "ansible_distribution_version", True) == "24.04"
+    ):
+        pytest.skip("Skipping this test on Ubuntu 24.04")
+
     service = host.service(get_variable(host, "fail2ban_service_name"))
 
     assert service.is_running
