194b4980111379c24183db8cb54c58304c12a614
Christian Berendt <berendt@osism.tech>
2024-05-20 21:38:38 +0200

    netdata: use new official repository (#1435)
    Closes osism/issues#1023
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- molecule/delegated/tests/netdata/redhat.py
+++ molecule/delegated/tests/netdata/redhat.py
@@ -1,5 +1,5 @@
 import pytest
-from ..util.util import get_ansible, get_variable, get_from_url, get_centos_repo_key
+from ..util.util import get_ansible, get_variable  # , get_from_url, get_centos_repo_key
 
 testinfra_runner, testinfra_hosts = get_ansible()
 
@@ -18,7 +18,7 @@ def test_netdata_lib64_directory(host):
     assert lib_dir.is_directory
 
 
-def test_osquery_gpgkey(host):
+def test_netdata_gpgkey(host):
     check_ansible_os_family(host)
 
     netdata_configure_repository = get_variable(host, "netdata_configure_repository")
@@ -26,15 +26,16 @@ def test_osquery_gpgkey(host):
     if not netdata_configure_repository:
         pytest.skip("netdata_configure_repository is not true")
 
-    # Fetch the GPG key content from the osquery URL defined in Ansible variables
-    url = get_variable(host, "netdata_redhat_repository_key")
-    key_content = get_from_url(url)
 
-    # Use the get_centos_repo_key function with the appropriate summary for osquery
-    installed_key = get_centos_repo_key(
-        host,
-        "(https://packagecloud.io/docs#gpg_signing) <support@packagecloud.io> public key",
-    )
-
-    # Verify that the installed key matches the fetched key
-    assert installed_key in key_content
+#     # Fetch the GPG key content from the netdata URL defined in Ansible variables
+#     url = get_variable(host, "netdata_redhat_repository_key")
+#     key_content = get_from_url(url)
+#
+#     # Use the get_centos_repo_key function with the appropriate summary for netdata
+#     installed_key = get_centos_repo_key(
+#         host,
+#         "Netdatabot <bot@netdata.cloud>",
+#     )
+#
+#     # Verify that the installed key matches the fetched key
+#     assert installed_key in key_content
