61cd457d379f0ebb4337f7efed7c241928edf5ec
Christian Berendt <berendt@osism.tech>
2024-05-31 18:20:42 +0200

    molecule: improve & fix netdata tests
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- molecule/delegated/tests/netdata/debian.py
+++ molecule/delegated/tests/netdata/debian.py
@@ -1,4 +1,5 @@
 import pytest
+
 from ..util.util import get_ansible, get_variable, get_from_url
 
 testinfra_runner, testinfra_hosts = get_ansible()
@@ -9,7 +10,7 @@ def check_ansible_os_family(host):
         pytest.skip("ansible_os_family mismatch")
 
 
-def test_repository_configuration(host):
+def test_netdata_repository_key(host):
     check_ansible_os_family(host)
 
     if not get_variable(host, "netdata_configure_repository"):
@@ -17,6 +18,7 @@ def test_repository_configuration(host):
 
     key_file = host.file("/etc/apt/trusted.gpg.d/netdata.asc")
     key_content = get_from_url(get_variable(host, "netdata_debian_repository_key"))
+
     assert key_file.exists
     assert not key_file.is_directory
     assert key_file.user == "root"
