4dc65c38106d18c70e0274defbcda3de6330d550
Christian Berendt <berendt@osism.tech>
2024-06-02 15:59:16 +0200

    molecule: unify clamav tests
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- molecule/delegated/tests/clamav/main.py
+++ molecule/delegated/tests/clamav/main.py
@@ -5,7 +5,7 @@ from ..util.util import get_ansible, get_variable, get_family_role_variable
 testinfra_runner, testinfra_hosts = get_ansible()
 
 
-def test_srv(host):
+def test_clamav_service(host):
     service = host.service(
         get_family_role_variable(host, "clamav_freshclam_service_name")
     )
@@ -19,7 +19,7 @@ def test_srv(host):
     assert service.is_enabled
 
 
-def test_configfile(host):
+def test_clamav_configuration_file(host):
     try:
         file_path = get_variable(host, "clamav_configuration_path")
     except Exception:
