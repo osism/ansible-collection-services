6661d648b7da6875ab201a43689255d42e90ca9a
Christian Berendt <berendt@osism.tech>
2024-06-20 09:29:15 +0200

    molecule: do not skip fail2ban tests on ubuntu 24.04 (#1466)
    It makes no sense to skip a unit test on a specific distro version.
    Either we have to skip the whole molecule test for Ubuntu 24.04 or
    the failed test is ok until upstream fixed the LP#2055114.
    
    The fail2ban checks on ubuntu 24.04 are currently disabled in Zuul.
    
    Signed-off-by: Christian Berendt <berendt@osism.tech>

--- molecule/delegated/tests/fail2ban.py
+++ molecule/delegated/tests/fail2ban.py
@@ -1,28 +1,16 @@
-import pytest
 from .util.util import get_ansible, get_variable
 
 testinfra_runner, testinfra_hosts = get_ansible()
 
 
-def test_srv(host):
-    """
-    Temporary skipping on Ubuntu 24.04 as the service is currently unstable on 24.04
-    Also see: https://bugs.launchpad.net/ubuntu/+source/fail2ban/+bug/2055114
-    """
-
-    if (
-        get_variable(host, "ansible_distribution", True) == "Ubuntu"
-        and get_variable(host, "ansible_distribution_version", True) == "24.04"
-    ):
-        pytest.skip("Skipping this test on Ubuntu 24.04")
-
+def test_fail2ban_service(host):
     service = host.service(get_variable(host, "fail2ban_service_name"))
 
     assert service.is_running
     assert service.is_enabled
 
 
-def test_pkg(host):
+def test_fail2ban_package(host):
     package_name = get_variable(host, "fail2ban_package_name")
     assert package_name != ""
 
