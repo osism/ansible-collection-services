4909b87e0d4193b2edc4d889d2079789ee37b5a5
gndrmnn <120007213+gndrmnn@users.noreply.github.com>
2024-10-15 16:41:00 +0200

    teleport: add Centos support (#1676)
    Signed-off-by: Gondermann <gondermann@b1-systems.de>

--- molecule/delegated/tests/teleport/debian.py
+++ molecule/delegated/tests/teleport/debian.py
@@ -1,5 +1,5 @@
 import pytest
-from ..util.util import get_ansible, get_variable
+from ..util.util import get_ansible, get_variable, get_from_url
 
 testinfra_runner, testinfra_hosts = get_ansible()
 
@@ -9,10 +9,23 @@ def check_ansible_os_family(host):
         pytest.skip("ansible_os_family mismatch")
 
 
-def test_teleport_apt_transport_https_package_installed(host):
+def test_teleport_repository(host):
     check_ansible_os_family(host)
 
     teleport_configure_repository = get_variable(host, "teleport_configure_repository")
-    if teleport_configure_repository:
-        apt_transport_https_package = host.package("apt-transport-https")
-        assert apt_transport_https_package.is_installed
+
+    if not teleport_configure_repository:
+        pytest.skip("teleport_configure_repository is not true")
+
+    package = host.package("apt-transport-https")
+    assert package.is_installed
+
+    key_file = host.file("/etc/apt/trusted.gpg.d/teleport.asc")
+    key_content = get_from_url(get_variable(host, "teleport_debian_repository_key"))
+
+    assert key_file.exists
+    assert not key_file.is_directory
+    assert key_file.user == "root"
+    assert key_file.group == "root"
+    assert key_file.mode == 0o644
+    assert key_file.content_string == key_content
