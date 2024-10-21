21cf7b53630c5db05a8f2f2ea5a5752e4dcb03ee
Christian Berendt <berendt@osism.tech>
2024-10-21 10:35:03 +0200

    Add wazuh-agent role
    Signed-off-by: Christian Berendt <berendt@osism.tech>

---
+++ molecule/delegated/tests/wazuh_agent/redhat.py
@@ -0,0 +1,13 @@
+import pytest
+from ..util.util import get_ansible, get_variable
+
+testinfra_runner, testinfra_hosts = get_ansible()
+
+
+def check_ansible_os_family(host):
+    if get_variable(host, "ansible_os_family", True) != "RedHat":
+        pytest.skip("ansible_os_family mismatch")
+
+
+def test_teleport_repository(host):
+    check_ansible_os_family(host)
