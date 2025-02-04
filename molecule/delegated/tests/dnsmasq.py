c5077d24510b00cab109e4e5d337244d11b6284d
Christian Berendt <berendt@osism.tech>
2025-02-04 21:09:26 +0100

    Add dnsmasq role
    Signed-off-by: Christian Berendt <berendt@osism.tech>

---
+++ molecule/delegated/tests/dnsmasq.py
@@ -0,0 +1,25 @@
+from .util.util import get_ansible, get_variable
+
+testinfra_runner, testinfra_hosts = get_ansible()
+
+
+def test_dnsmasq_directories(host):
+    directories = [
+        get_variable(host, "dnsmasq_docker_compose_directory"),
+        get_variable(host, "dnsmasq_configuration_directory"),
+    ]
+
+    for d in directories:
+        f = host.file(d)
+        assert f.exists
+        assert f.is_directory
+        assert f.mode == 0o750
+        assert f.user == get_variable(host, "operator_user")
+        assert f.group == get_variable(host, "operator_group")
+
+
+def test_dnsmasq_service(host):
+    service = host.service(get_variable(host, "dnsmasq_service_name"))
+
+    assert service.is_running
+    assert service.is_enabled
